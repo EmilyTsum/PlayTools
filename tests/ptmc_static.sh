eb52da565f7a44ef48dcac81272ce893cec5f5fb
EmilyTsum <297192186+EmilyTsum@users.noreply.github.com>
2026-08-15 09:59:22 +0000

    test: validate jitter recovery paths

--- tests/ptmc_static.sh
+++ tests/ptmc_static.sh
@@ -70,6 +70,7 @@ grep -q 'Deadline-based sampling' "$INC"
 
 grep -q 'presentFPS' "$INC"
 grep -q 'captureFPS' "$INC"
-grep -q 'encodedFPS' "$INC"grep -q 'sourceClearlyFaster' "$INC"
+grep -q 'encodedFPS' "$INC"
+grep -q 'sourceClearlyFaster' "$INC"
 grep -q 'burstSlotUses' "$INC"
 grep -q 'allocatedSlotCount = session.bufferCount + 1' "$INC"
